ea61fc1693e2b15bf1fcdd5fc21cc07c609a8088
theophile <chmeredith@gmail.com>
2021-02-23 19:52:54 -0600

    Updates to Fluidsynth module

--- includes/fluidsynth.py
+++ includes/fluidsynth.py
@@ -1,44 +1,54 @@
-
 import fluidsynth
 import time, os, sys
 import threading
+import logging
 from sf2utils.sf2parse import Sf2File
 
-class Fluidsynth(object):
+class Fluidsynth:
     def __init__(self):
-        self.fs = self.startFluidsynth()
         self.Channel = 0
         self.SF2dir= '/home/pi/soundfonts/sf2/'
         self.SF2paths = {}
         self.SF2Path = self.buildSF2List()
-        self.sfid = self.fs.sfload(self.SF2Path)
         self.BankPatchList = self.getSF2bankpatchlist(self.SF2Path)
-        self.switchSF2(self.SF2Path, 0, 0, 1)
+
+        self.name = "FluidSynth"
+        self.driver='jack'
+        self.midi_driver='jack'
+        self.command_prompt = "\nFluidSynth initialization completed."
+
+    def __del__(self):
+        self.stop()
+        print('Destructor called, Fluidsynth stopped.')
+
+    def start(self, sf2path=None):
+        if not sf2path:
+            sf2path = self.SF2Path
+        logging.info("Starting Engine {}".format(self.name))
+        self.fs = fluidsynth.Synth(gain=0.75, samplerate=48000)
+        self.fs.setting('audio.jack.autoconnect', 1)
+        self.fs.setting('synth.polyphony', 32)
+        self.fs.setting('audio.period-size', 64)
+        self.fs.setting('audio.periods', 4)
+        self.fs.setting('audio.realtime-prio', 99)
+        self.fs.start(driver=self.driver, midi_driver=self.midi_driver)
+        self.sfid = self.fs.sfload(sf2path)
+        self.switchSF2(sf2path, 0, 0, 1)
         self.Bank = self.fs.channel_info(self.Channel)[1]
         self.Patch = self.fs.channel_info(self.Channel)[2]
         self.PatchName = self.fs.channel_info(self.Channel)[3]
         self.Index = self.BankPatchList.index([self.Bank, self.Patch])
-
-
-    def __del__(self):
-        self.fs.delete()
-        print('Destructor called, Fluidsynth stopped.')
-
-
-    def startFluidsynth(self):
-        fs = fluidsynth.Synth(gain=1, samplerate=48000)
-        fs.setting('synth.polyphony', 32)
-        #fs.setting('audio.period-size', 6)
-        fs.setting('audio.periods', 4)
-        fs.setting('audio.realtime-prio', 99)
-        fs.start(driver='alsa', midi_driver='alsa_seq')
         print('fluidsynth running...')
-        return fs
+        return
 
+    def stop(self):
+        self.fs.delete()
+        return
 
     def buildSF2List(self):
         for file in os.listdir(self.SF2dir):
             if file[-4:].lower() == ".sf2":
+                print(file)
                 self.SF2paths.update({file[:-4]: (self.SF2dir + file)})
         return self.SF2paths[list(self.SF2paths.keys())[0]]
 
